539579a4bcd8cba39754a977a2cbced6e7c68ed9
Lucas Dearaujo <lucasaraujo45@outlook.com>
2023-08-31 09:10:56 -0400

    Create setCartCookie.js

---
+++ get-and-transfer-cart-cookie/setCartCookie.js
@@ -0,0 +1,4 @@
+document.cookie = "cart=YOUR_CART_TOKEN_HERE; path=/";
+console.log("Cart Token Set!");
+
+//the cart cookie should be the string of characters you got from running getCartCookie script
